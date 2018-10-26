0e609c7804f736b23392d9cc766715159f3c34af
Sig <drakonsmert@gmail.com>
2018-10-27 00:26:10 +0300

    lazy_py:  - raw string fix

--- example/test.py
+++ example/test.py
@@ -17,7 +17,7 @@ r''' Copyright 2018, SigDev
 
 import os
 import sys
-sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')))
+sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), r'..')))
 import lazy
 import pprint
 
